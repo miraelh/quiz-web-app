47f63503bbe029e93aa07338cab01de2dfe0ae0b
Mira E <miraelhajj0@gmail.com>
2025-04-21 01:40:02 +0300

    Adding the inputs for username and password

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const username = document.getElementById('loginUsername').value;
+    const password = document.getElementById('loginPassword').value;
+});  
