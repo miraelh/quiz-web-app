aaa255378f79b88c22764316231e8faf886f1297
Mira E <miraelhajj0@gmail.com>
2025-04-21 01:42:30 +0300

    Adding the error message

--- login.js
+++ login.js
@@ -3,4 +3,5 @@ document.getElementById('loginForm').addEventListener('submit', function(e) {
   
     const username = document.getElementById('loginUsername').value;
     const password = document.getElementById('loginPassword').value;
+    const errorMessage = document.getElementById('error-message');
 });  
