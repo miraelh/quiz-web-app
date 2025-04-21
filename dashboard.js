457ac43d5b21fdec0eb7905f7d10f65e394124aa
Mira E <miraelhajj0@gmail.com>
2025-04-21 17:46:33 +0300

    Getting the user scores from localStorage

--- dashboard.js
+++ dashboard.js
@@ -4,4 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
         window.location.href = 'index.html';
         return;
     }
+
+    const userScores = JSON.parse(localStorage.getItem('userScores')) || {};
+    const tableBody = document.querySelector('tbody');
+
 })
