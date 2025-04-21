33633c7666f05ec643d9d4b1a68c951d482501b0
Mira E <miraelhajj0@gmail.com>
2025-04-21 17:45:10 +0300

    Check if the  user logging in is the admin and redirecting to home page if not

--- dashboard.js
+++ dashboard.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const loggedInUser = JSON.parse(localStorage.getItem('loggedInUser'));
-    
+    if (!loggedInUser || loggedInUser.username !== 'admin@quiz.com') {
+        window.location.href = 'index.html';
+        return;
+    }
 })
